c85bdc6fd67fea274090d0f9f153cd053f8768cd
おさぺこ <soraurikyufmof@gmail.com>
2020-11-29 17:38:01 +0900

    変更点などのコメント

---
+++ app/src/main/java/com/example/imageauth/Activity_Content.kt
@@ -0,0 +1,11 @@
+package com.example.imageauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Content : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__content)
+    }
+}
---
+++ app/src/main/java/com/example/imageauth/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.imageauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //ラベル認証画面ボタン押下
+    fun onLblButtonTapped(view: View?){
+        val intent = Intent(this, Activity_Label::class.java)
+        startActivity(intent)
+    }
+
+    //コンテンツ節度測定ボタン押下
+    fun onContentButtonTapped(view: View?){
+        val intent = Intent(this, Activity_Content::class.java)
+        startActivity(intent)
+    }
+    //テキスト検出ボタン押下
+    fun onTextButtonTapped(view: View?){
+        val intent = Intent(this, Activity_Text::class.java)
+        startActivity(intent)
+    }
+   // 顔検出と分析ボタン押下
+   fun onFaceButtonTapped(view: View?){
+       val intent = Intent(this, Activity_Face::class.java)
+       startActivity(intent)
+   }
+    //有名人の認識ボタン押下
+    fun onCelebrityButtonTapped(view: View?){
+        val intent = Intent(this, Activity_Celebrity::class.java)
+        startActivity(intent)
+    }
+}
